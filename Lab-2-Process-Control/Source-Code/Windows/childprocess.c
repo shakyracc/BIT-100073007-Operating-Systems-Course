92f360c220ea56f312baa35ebdd90d994c8364ae
shak <shakyracornelius@gmail.com>
2022-12-15 16:05:07 -0400

    Add Lab2 Windows sourcecode

---
+++ Lab-2-Process-Control/Source-Code/Windows/childprocess.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main () {
+printf("Hello my name is Xiaqila\n");
+printf("Delay 3s\n");
+Sleep(3000);
+return 0;
+}
---
+++ Lab-2-Process-Control/Source-Code/Windows/parentprocess.c
@@ -0,0 +1,54 @@
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+
+void _tmain(int argc, TCHAR *argv[])
+{
+
+    SYSTEMTIME st, et; // pointer to a SYSTEMTIME structure to receive the current system date and time.
+
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    if (argc != 2)
+    {
+        printf("Usage: %s [cmdline]\n", argv[0]);
+        return;
+    }
+
+    // Start the child process.
+    if (!CreateProcess(NULL,    // No module name (use command line)
+                       argv[1], // Command line
+                       NULL,    // Process handle not inheritable
+                       NULL,    // Thread handle not inheritable
+                       FALSE,   // Set handle inheritance to FALSE
+                       0,       // No creation flags
+                       NULL,    // Use parent's environment block
+                       NULL,    // Use parent's starting directory
+                       &si,     // Pointer to STARTUPINFO structure
+                       &pi)     // Pointer to PROCESS_INFORMATION structure
+    )
+    {
+        printf("CreateProcess failed (%d).\n", GetLastError());
+        return;
+    }
+
+    GetSystemTime(&st);
+    printf("The child process start time is: %02dh:%02dm:%02ds:%02dms\n", st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
+
+    // Wait until child process exits.
+    printf("Waiting for 3 seconds\n");
+    WaitForSingleObject(pi.hProcess, 3000);
+    
+    GetSystemTime(&et);
+    printf(" The child process end time is: %02dh:%02dm:%02ds:%02dms\n", et.wHour, et.wMinute, et.wSecond, et.wMilliseconds);
+
+    // Close process and thread handles.
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
