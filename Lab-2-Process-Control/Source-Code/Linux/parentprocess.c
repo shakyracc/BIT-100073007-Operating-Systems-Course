73586af3bf9ae487bfea6796837398f2ea527ac2
shakyracc <shakyracornelius@gmail.com>
2022-12-15 20:32:30 -0400

    corrected start time and elapsed time calculations

--- Lab-2-Process-Control/Source-Code/Linux/parentprocess.c
+++ Lab-2-Process-Control/Source-Code/Linux/parentprocess.c
@@ -7,14 +7,12 @@
 
 int main (int argc, char *argv[]) {
 
-	struct timeval start, end;
+	struct timeval start, end, start2;
 
 	pid_t pid;
 	pid = fork();
 
 	if(pid==0) {
-		gettimeofday(&start, NULL);
-		printf("Start child process\n");
 		int sts_cd = execv("/home/shak/Documents/BIT-100073007-Operating-Systems-Course/Lab-2-Process-Control/Source-Code/Linux/childprocess", argv);
 		printf("There is an issues with the running command\n");
 		if (sts_cd == -1) {
@@ -23,11 +21,12 @@ int main (int argc, char *argv[]) {
 		}
 	}
 	else if ( pid > 0 ) {
+		gettimeofday(&start, NULL);
+		printf("The child process start time is: %ld seconds %ld micro seconds\n", start.tv_sec, start.tv_usec);
 		int wc = wait(NULL);
 		gettimeofday(&end, NULL);
-		printf("Parent process of %d, (wc:%d), (pid:%d)\n", pid, wc, (int)getpid());
-		printf("Child process: %d startTime: %ld seconds %ld micro seconds, endTime: %ld seconds %ld micro seconds , elapsedTime:  %ld micro seconds\n",
-			 wc, start.tv_sec, start.tv_usec, end.tv_sec, end.tv_usec, ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+		printf("The child process end time is: %ld seconds %ld micro seconds\n", end.tv_sec, end.tv_usec);
+		printf("The child process elapsed time is: %lds.%ldms\n", (end.tv_sec - start.tv_sec), (end.tv_usec - start.tv_usec) / 1000);
 	}
 	else {
 		printf("Error while forking \n");
