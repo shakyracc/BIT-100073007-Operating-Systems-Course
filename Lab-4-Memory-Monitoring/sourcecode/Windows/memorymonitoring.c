598d5d9906a9f64d900881cf04e6b1d5e7ac097d
shak <shakyracornelius@gmail.com>
2022-12-17 01:46:45 -0400

    add windows sourcecode and GetSystemInfo function

---
+++ Lab-4-Memory-Monitoring/sourcecode/Windows/memorymonitoring.c
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <stdio.h>
+#pragma comment(lib, "user32.lib")
+
+void main()
+{
+   SYSTEM_INFO siSysInfo;
+ 
+   // Copy the hardware information to the SYSTEM_INFO structure. 
+ 
+   GetSystemInfo(&siSysInfo); 
+ 
+   // Display the contents of the SYSTEM_INFO structure. 
+
+   printf("Hardware information: \n");  
+   printf("  Number of processors: %u\n", 
+      siSysInfo.dwNumberOfProcessors); 
+   printf("  Page size: %u\n", siSysInfo.dwPageSize); 
+   printf("  Processor type: %u\n", siSysInfo.dwProcessorType); 
+   printf("  Allocation Granularity: %u\n", siSysInfo.dwAllocationGranularity);
+
+   return 0; 
+}
